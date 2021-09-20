d2d8b44d4d65ec5e3c7dd9914af0d53671d7285c
watcol <potfman@gmail.com>
2021-09-20 10:19:42 +0900

    Add structopt

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opts {
+    #[structopt(
+        name = "FILE",
+        help = "The entry configuration file",
+        default_value = "farbe/main.toml"
+    )]
+    entry: PathBuf,
+}
+
+fn main() {
+    let opts = Opts::from_args();
+    println!("{:?}", opts);
+}
